f51456ea00ae4eb8b16864fc15c70a2e8533a8e8
OovEver <mupengfei6688@163.com>
2017-03-20 13:42:23 +0800

    修改对第三类EXCEL表格的存入，使程序可以适应多个合并单元格的情况，增加程序的健壮性

--- ExcelToJson/routes/readExcelForThree.js
+++ ExcelToJson/routes/readExcelForThree.js
@@ -9,8 +9,10 @@ var xlsx = require('node-xlsx');
 //加载文件读取包
 var fs = require('fs');
 var row;//记录行
-var col=-1;//记录列
-var max_col=0;//最大记录列
+var col=[];//记录列
+var max_col=[];//最大记录列
+var flag=-1;//标记位，用来标记是否有合并单元格
+var col_num=0;//记录合并单元格的数目
 function readExcel(fileName,tableHead,JsonName) {
     var obj = xlsx.parse(fileName);//配置excel文件的路径
 
@@ -29,18 +31,10 @@ function readExcel(fileName,tableHead,JsonName) {
     //由于表格内容中表头一般都小于5但是表格中内容都大于5所以用长度为5去判断是否为表头，如果为表头，则跳过表头同时跳过表的第一格（说明）
     //可根据实际情况调整length大小
     if (excelObj[i].length < 5) {
-        i = 2;
-    } else {//如果没有表头说明的情况下，把表的第一行说明跳过，如果不需要跳过，则此处i改为0即可
         i = 1;
+    } else {//如果没有表头说明的情况下，把表的第一行说明跳过，如果不需要跳过，则此处i改为0即可
+        i = 0;
     }
-    //判断如果是表的说明则进行i++
-    //因为每个表的第一个说明都是编号信息，所以用编号模糊匹配可以判断是否为表的说明
-    if (typeof excelObj[i][0] != "number" == true) {
-        if (excelObj[i][0] != null && excelObj[i][0].indexOf('号') > 0) {
-            i++;
-        }
-    }
-
     for( i=i;i<excelObj.length;i++){
         var arr=[];
         var key=[];
@@ -48,30 +42,53 @@ function readExcel(fileName,tableHead,JsonName) {
         var value=excelObj[i];
         var j=0;
         var k=0;//表头下标
-
+        var col_temp=0;
+        var max_col_temp=0;
         var joinString="";//连接后的字符串
         while(value[j]==null)//过滤excel前的空格
             j++;
-        //如果第一行不为0 则代表前面有空格 为了防止是表格漂移导致的不为0的情况，再判断一是否上一列小于下一列，如果小于则为合并单元格
-        if(j!=0&&excelObj[i].length<excelObj[i+1].length&&i+1<excelObj.length)
-        {
-            row=i;//将当前行赋予row
-            col=j;//将当前有值的列赋值给col以便记录
-            max_col=excelObj[i].length;//最后一个有值元素的位置
-            continue;
-        }
-
-        for(j=j;j<=value.length-1;j++){
-            if(j==col&&col!=-1){
-                for(j;j<max_col;j++){
-                    joinString+=excelObj[row][j]+":"+value[j]+",";//对列中的字符串进行拼接
+        //如果第一行不为0 则代表前面有空格
+        if(flag==-1) {
+            row = i;//将当前行赋予row
+            for (j = 0; j < excelObj[i].length;) {
+                col_temp = j;//将当前有值的列赋值给col以便记录
+                j++;
+                //用于判断此个合并单元格有多少子单元格
+                while (j < excelObj[row].length&&excelObj[row][j] == null) {
+                    j++;
                 }
+                max_col_temp = j;//最后一个有值元素的位置
+                if (max_col_temp > col_temp + 1) {
+                    col[col_num]=col_temp;
+                    max_col[col_num]=max_col_temp;
+                    col_num++;
+                    flag = 1;
+                }
+
+
+            }
+            if(flag==1)
+            {
+                i++;
+                continue;
+            }
+        }
+        var m=0;
+        for(j=j;j<=value.length-1;j++){
+            if(m<col.length&&j==col[m]&&col_num>0){
+                joinString="";
+                for(j;j<max_col[m]-1;j++){
+                    joinString+=excelObj[row+1][j]+":"+value[j]+",";//对列中的字符串进行拼接
+                }
+				joinString+=excelObj[row+1][j]+":"+value[j];
                 value[--j]=joinString;
+                m++;
 
             }
             excelObject[tableHead[k]]=value[j];
            console.log(i+"行"+j+"列"+value[j]+" "+tableHead[k]);
             k++;
+
 
         }
         //若还有null的数据为存入data，进行存入
@@ -80,7 +97,7 @@ function readExcel(fileName,tableHead,JsonName) {
         }
         data.push(excelObject);
     }
-    console.log(data);
+     console.log(data);
     //将对象转化为字符串
     writeFile(JsonName,JSON.stringify(data));
 }
